d3fb09ff554e4b5c4e0909bff952290baea5640e
Alucard-2291 <saurabhs1304@gmail.com>
2020-11-22 17:32:58 +0530

    Creating shop page with all data

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-import HatsPage from './pages/hatspage/hatspage.component';
+// import HatsPage from './pages/hatspage/hatspage.component';
+import ShopPage from './pages/shoppage/shoppage.component';
 
 function App() {
   return  (
     <div>
       <Switch>
         <Route exact path='/' component= {HomePage} />
-        <Route path='/hats' component= {HatsPage} />
+        <Route path='/shop' component= {ShopPage} />
       </Switch>
     </div>
   );
