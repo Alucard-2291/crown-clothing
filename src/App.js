a777b29be7e5cde3a2bb0b9f9a4a04b416677e55
Alucard-2291 <saurabhs1304@gmail.com>
2020-11-23 16:02:38 +0530

    created and styled sign-in form

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import HomePage from './pages/homepage/homepage.component';
 // import HatsPage from './pages/hatspage/hatspage.component';
 import ShopPage from './pages/shoppage/shoppage.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/signin-signup/signin-signup.component';
 
 function App() {
   return  (
@@ -15,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path='/' component= {HomePage} />
         <Route path='/shop' component= {ShopPage} />
+        <Route path='/signin' component= {SignInAndSignUp} />
       </Switch>
     </div>
   );
