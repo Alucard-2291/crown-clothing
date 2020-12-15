184f4cd564f85b1347d9bd0e452ed7f3c6da07f0
Alucard-2291 <saurabhs1304@gmail.com>
2020-12-15 14:30:11 +0530

    completed sign in with email and password

--- src/App.js
+++ src/App.js
@@ -32,9 +32,7 @@ class App extends Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          })
-
-          console.log(this.state);
+          });
         });
       } else {
         this.setState({currentUser: userAuth});
